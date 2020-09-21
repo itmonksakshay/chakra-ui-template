0c0ba82ecd535b1716430f2ef24612a755e361c2
itmonksakshay <akshay.bhatt@hotmail.com>
2020-09-21 13:04:00 +0530

    server is added

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express= require('express');
+app=express();
+app.use(express.static('./dist'));
+app.get('*', function(request,response) {
+	response.sendFile(path.join(__dirname,"/../dist/index.html"));
+});
+app.listen(3000,function(){
+	console.log("the server is running on port 4000");
+});
