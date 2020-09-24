e9d24bb69732dc669cdfadb865eefc85db8f2e27
itmonksakshay <akshay.bhatt@hotmail.com>
2020-09-24 15:20:56 +0530

    Top Nav is completed

--- src/theme-files/header/topNav.jsx
+++ src/theme-files/header/topNav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Box,Stack,PseudoBox,Link} from '@chakra-ui/core';
-
+import {BiPhoneCall,BiLogIn} from 'react-icons/bi';
+import {AiOutlineMail,AiOutlineUserAdd} from 'react-icons/ai';
 const ListItem=(props)=>(<PseudoBox as="li" 
   display="inline" 
   borderRight={1} 
@@ -9,14 +10,28 @@ const ListItem=(props)=>(<PseudoBox as="li"
   pt="0.1rem"
   pr="0.5rem"
   pl="0.5rem"
-  pb="0.1rem"{...props}>
-  <Link href="#" color="primary.10" textDecoration="none">{props.children}</Link>
+  pb="0.1rem"
+  _last={{borderRight:0}}
+  {...props}>
+  <Link href="#" color="primary.10" 
+    textDecoration="none" 
+    _hover={{textDecoration:"none",color:"white"}} 
+    _focus={{textDecoration:"none",color:"white"}}>
+  {props.children}</Link>
   </PseudoBox>);
 
-const  LeftNav =()=>(<Box flex="basis" w="auto" maxW="none" pos="relative" minH={1} pr={15} pl={15} mr="auto">
+const RightNav=()=>(<Box flexBasis="auto" w="auto" maxW="100%" pos="relative" minH={1} pr={15} pl={15}>
     <Stack as="ul" display="block" listStyleType="none" m={0} p={0 } fontSize={13} fontFamily="Open Sans, sans-serif">
-      <ListItem>+91 9113923240</ListItem>
-      <ListItem>codepluck@codepluck.com</ListItem>
+    <ListItem><AiOutlineUserAdd style={{display:"inline-block",marginRight:"0.5rem"}} />Register</ListItem>
+    <ListItem><BiLogIn style={{display:"inline-block",marginRight:"0.5rem"}} />Login</ListItem>
+    </Stack>
+  </Box>);
+
+
+const  LeftNav =()=>(<Box flexBasis="auto" w="auto" maxW="100%" pos="relative" minH={1} pr={15} pl={15} mr="auto">
+    <Stack as="ul" display="block" listStyleType="none" m={0} p={0 } fontSize={13} fontFamily="Open Sans, sans-serif">
+      <ListItem><BiPhoneCall style={{display:"inline-block",marginRight:"0.5rem"}}/> +91 9113923240</ListItem>
+      <ListItem><AiOutlineMail style={{display:"inline-block",marginRight:"0.5rem"}}/>codepluck@codepluck.com</ListItem>
     </Stack>
  </Box>);
 
@@ -33,6 +48,7 @@ export default function TopNav(){
     display ={["","","block",""]}>
     <Box display="flex" mr={-15} ml={-15} flexWrap="wrap">
      <LeftNav/>
+     <RightNav/>
     </Box>
   </Box>);
 }
