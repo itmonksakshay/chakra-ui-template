f8c595e5878b9080d87d24e0a5f9d206480bef4e
itmonksakshay <akshay.bhatt@hotmail.com>
2020-10-12 21:38:14 +0530

    slider is added

--- src/theme-files/header/index.js
+++ src/theme-files/header/index.js
@@ -9,19 +9,21 @@ import Nav from './mainNav';
 const MainHeader =(props)=>(<Box as="header" display="flex" mr={-15} ml={-15} flexWrap="wrap" {...props}/>);
 export default class Header extends Component {
   render(props){
-     return(<MainHeader {...props}>
-      <TopNav/>
-      <Box pt="1.5rem" bg="white" flexBasis="100%" maxW="100%" pos="relative" w="100%" pr={15} pl={15}>
-       <Box display="flex" flexWrap="wrap" mr={-15} ml={-15}>
-        <HeaderLogo/>
-        <SearchBox/>
-        <HeaderWidget>  
-          <MobileMenu/>
-        </HeaderWidget>
-       </Box>
-       <Nav/>
-      </Box>
-     </MainHeader>);
+     return(<Box pos="relative" w="100%" pr={15} pl={15} maxW="100%" flexBasis="100%">
+       <MainHeader {...props}>
+        <TopNav/>
+        <Box pt="1.5rem" bg="white" flexBasis="100%" maxW="100%" pos="relative" w="100%" pr={15} pl={15}>
+          <Box display="flex" flexWrap="wrap" mr={-15} ml={-15}>
+            <HeaderLogo/>
+            <SearchBox/>
+            <HeaderWidget>  
+              <MobileMenu/>
+            </HeaderWidget>
+          </Box>
+          <Nav/>
+        </Box>
+       </MainHeader>
+      </Box>);
   }
 
 }
--- src/theme-files/index.js
+++ src/theme-files/index.js
@@ -5,14 +5,12 @@ import Footer from './footer';
 export default class Index extends Component {
   render(props){
     return(<Box w="100%" pr={15} pl={15} mr="auto" ml="auto" {...props}>
-        <Box display="flex" mr={-15} ml={-15} flexWrap="wrap" minH="100vh">
-         <Box pos="relative" w="100%" pr={15} pl={15} maxW="100%" flexBasis="100%">
-          <Header/>
-          {this.props.children}
-          <Footer/>
-         </Box>
-        </Box>
-      </Box>);
+          <Box display="flex" mr={-15} ml={-15} flexWrap="wrap" minH="100vh">
+            <Header/>
+            {this.props.children}
+            <Footer/>
+          </Box>
+        </Box>);
   }
 
 }
